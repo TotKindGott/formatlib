a8858a9aec261b32ee2e0f2bc11e4faf8293dca9
Konstantin A Kolosov <konstantin.a.kolosov@gmail.com>
2016-09-11 10:58:52 -0400

    Create states.py

---
+++ states.py
@@ -0,0 +1,86 @@
+# coding: utf-8
+
+us_states = {
+  'AK': 'Alaska',
+  'AL': 'Alabama',
+  'AR': 'Arkansas',
+  'AS': 'American Samoa',
+  'AZ': 'Arizona',
+  'CA': 'California',
+  'CO': 'Colorado',
+  'CT': 'Connecticut',
+  'DC': 'District of Columbia',
+  'DE': 'Delaware',
+  'FL': 'Florida',
+  'GA': 'Georgia',
+  'GU': 'Guam',
+  'HI': 'Hawaii',
+  'IA': 'Iowa',
+  'ID': 'Idaho',
+  'IL': 'Illinois',
+  'IN': 'Indiana',
+  'KS': 'Kansas',
+  'KY': 'Kentucky',
+  'LA': 'Louisiana',
+  'MA': 'Massachusetts',
+  'MD': 'Maryland',
+  'ME': 'Maine',
+  'MI': 'Michigan',
+  'MN': 'Minnesota',
+  'MO': 'Missouri',
+  'MP': 'Northern Mariana Islands',
+  'MS': 'Mississippi',
+  'MT': 'Montana',
+  'NA': 'National',
+  'NC': 'North Carolina',
+  'ND': 'North Dakota',
+  'NE': 'Nebraska',
+  'NH': 'New Hampshire',
+  'NJ': 'New Jersey',
+  'NM': 'New Mexico',
+  'NV': 'Nevada',
+  'NY': 'New York',
+  'OH': 'Ohio',
+  'OK': 'Oklahoma',
+  'OR': 'Oregon',
+  'PA': 'Pennsylvania',
+  'PR': 'Puerto Rico',
+  'RI': 'Rhode Island',
+  'SC': 'South Carolina',
+  'SD': 'South Dakota',
+  'TN': 'Tennessee',
+  'TX': 'Texas',
+  'UT': 'Utah',
+  'VA': 'Virginia',
+  'VI': 'Virgin Islands',
+  'VT': 'Vermont',
+  'WA': 'Washington',
+  'WI': 'Wisconsin',
+  'WV': 'West Virginia',
+  'WY': 'Wyoming'
+}
+
+
+def state_expand(state):
+  
+    try:
+        return us_states[state.upper()]
+    except:
+        if state in us_states.values():
+            return state
+        else:
+            raise Exception('not a valid state')
+
+
+def state_shorten(state):
+  
+    try:
+        abbreviation = (abbreviation for abbreviation, expansion in us_states.items() if expansion == state.title()).next()
+        return abbreviation
+    except:
+        if state in us_states.keys():
+            return state
+        else:
+            raise Exception('not a valid state')
+ 
+ 
